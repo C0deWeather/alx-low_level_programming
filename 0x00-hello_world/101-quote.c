ed371290edc394b7b7d33bee95684c812027a2c9
C0deWeather <bflloyd1653@gmail.com>
2025-09-11 19:37:24 +0100

    Updated the program to compute the length of the string locally without using strlen

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,8 +10,13 @@
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	int len = 0;
 
-	write(2, str, strlen(str));
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	write(2, str, len);
 
 	return (1);
 }
